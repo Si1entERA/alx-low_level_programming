3130127f8ab99841dfc7b2aa66b628ac2541cbe6
Si1entERA <NeoLegote@Outlook.com>
2023-03-17 11:10:16 -0700

    task 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 1; x < 10; x++)
+		{
+			if (i < x && i != x)
+			{
+				putchar(x + '0');
+				putchar(x + '0');
+
+				if (i + x != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
