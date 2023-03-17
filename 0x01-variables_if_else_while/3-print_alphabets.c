247371ed68f8e097b3af08047cc72c72089116d1
Si1entERA <NeoLegote@Outlook.com>
2023-03-17 09:48:19 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,13 +13,12 @@ int main(void)
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
-		putchar('\n');
 	}
 
 	for (i = 'A'; i <= 'Z'; i++)
 	{
 		putchar(i);
-		putchar('\n');
 	}
+	putchar(i);
 	return (0);
 }
