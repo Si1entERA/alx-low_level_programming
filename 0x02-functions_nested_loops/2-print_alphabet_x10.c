7e42c453513ccd4a0343b3917a3e3abcd9195a51
Si1entERA <NeoLegote@Outlook.com>
2023-03-21 04:11:59 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that prints 10x alphabets
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char x;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
